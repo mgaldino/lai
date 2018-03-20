6b028229e3efdef898c5722f93c6e5fccff3f7d9
mgaldino <mcz.fea@gmail.com>
2018-03-20 14:35:55 -0300

    Exemplo funcionando com uma base de pedido

--- lda_pedidos.R
+++ lda_pedidos.R
@@ -1,7 +1,3 @@
-install.packages("installr") 
-library(installr) 
-updateR()
-
 
 ##########################################
 ## LDA Pedidos do Achados e Pedidos ######
@@ -10,38 +6,129 @@ updateR()
 
 # pacotes
 library(XML)
-library(MCMCpack)}
 library(tm)
-if(require(SnowballC) == F){install.packages('SnowballC'); library(SnowballC)}
+library(SnowballC)
 library(dplyr)
-if(require(ggplot2) == F){install.packages('ggplot2'); library(ggplot2)}
-if(require(seqinr) == F) {install.packages("seqinr"); library(seqinr)}
-if(require(RTextTools) == F) {install.packages("RTextTools"); library(RTextTools)}
-if(require(topicmodels) == F) {install.packages("topicmodels"); library(topicmodels)}
+ library(ggplot2)
+library(seqinr)
+library(RTextTools)
+library(topicmodels)
+library(data.table)
+library(devtools)
+devtools::install_github("sfirke/janitor")
+library(janitor)
+devtools::install_github("mgaldino/tbaep")
+library(tbaep)
+
+setwd("C:\\Users\\mgaldino\\2018\\Geral TB\\Mailing")
+
+pedidos <- fread("govmg15.csv", encoding="UTF-8")
+
+# remove acentos, lowecase etc.
+pedidos <- pedidos %>%
+  clean_names() %>%
+  mutate(descricao_demanda = snakecase::to_any_case(descricao_demanda, case = "none",
+                                                    transliterations = c("Latin-ASCII")))
+
+
+## baixar stopwords do link:
+## https://raw.githubusercontent.com/stopwords-iso/stopwords-pt/master/stopwords-pt.txt
+
+pt_stop_words <- fread("stopwords-pt.txt")
+
+pedidos1 <- pedidos %>%
+  select(descricao_demanda)
+  
+
+## precisa remover acento antes
+
+# dtm.control <- list(
+#   tolower = T,
+#   removePunctuation = T,
+#   removeNumbers = T,
+#   stopwords = c(stopwords("portuguese"),pt_stop_words),
+#   stemming = T,
+#   wordLengths = c(3,Inf),
+#   weighting = weightTf
+# )
+
+# transforma DF em vetor 
+pedidos2 <- pedidos1$descricao_demanda 
+
+ped <- Corpus(VectorSource(pedidos2)) # , readerControl = dtm.control
+ped
+inspect(ped[1:2])
+# my_stopwords <- unique(unlist(c(stopwords("portuguese"),pt_stop_words)))
+
+ped <- tm_map(ped, removeNumbers)
+ped <- tm_map(ped, removePunctuation)
+ped <- tm_map(ped , stripWhitespace)
+ped <- tm_map(ped, tolower)
+ped <- tm_map(ped, removeWords, stopwords("portuguese") ) # demora um pouco
+ped <- tm_map(ped, stemDocument, language = "portuguese")
+
+inspect(ped[1:2])
+dtm.control <- list(wordLengths = c(2,Inf),
+                     weighting = weightTf)
+
+dtm <- DocumentTermMatrix(ped, control = dtm.control)
+dim(dtm)
+inspect(dtm[1:10,1:20])
+
+## cada doc é uma linha
+# cada palavra uma coluna
+# número de colunas e igual número de palavras únicas em todos os docs
+
+
+freq_words <- rowSums(as.matrix(dtm)) # Quantas palavras cada documento (linha) tem
+index <- which(freq_words==0) # índice de documentos em que não há palavras
+# exemplo: 4600
+inspect(dtm[4600, 1:10])
+
+
+inspect(dtm1[1:10, 1:10])
+
+
+dtm1 <- dtm[-index, ] # remove # palavras que não ocorrem em nenhum documento.
+dim(dtm1)
+
+# a partir daqui começa LDA
+
+set.seed(51)
+trainpoints <- sample(1:nrow(dtm1), 1*nrow(dtm1),replace=F) # to train on a subsample, change 1.0 to a lower value, say 0.8
+
+k <- 10 # número de tópicos. Estou chutando
+
+## função pra extrair termos
+SpecificTerms <- function(lda.model,n=1) {
+  p <- posterior(lda.model)$terms
+  n <- min(n,ncol(p))
+  cumulativep <- colSums(p)
+  specificp <- t(apply(p,1,function(x) ((x) + (x/cumulativep) )))
+  
+  topterms <- t(apply(specificp, 1, function(x)
+    (colnames(specificp)[order(x,decreasing=T)[1:n]]) ))
+  
+  topterms
+}
+
+set.seed(2)
+system.time(lda1 <- LDA(dtm1, k))
+
+# t termos mais prováveis por tópico
+t <- 15
+View(terms(lda, t))
+
+## vamos olhar o Topic 17
+## Aprov Eleitoral? Bush, Clinton etc.
 
 
 
-data(NYTimes)
-dim(NYTimes)
-nyt <- NYTimes
-
-extendedstopwords <- c("a","about","above","across","after",
-                       "again","against","all","almost",
-                       "alone","along","already","also",
-                       "although","always","am","among",
-                       "an","and","another","any","anybody","anyone","anything","anywhere","are","area","areas","aren't","around","as","ask","asked","asking","asks","at","away","b","back","backed","backing","backs","be","became","because","become","becomes","been","before","began","behind","being","beings","below","best","better","between","big","both","but","by","c","came","can","cannot","can't","case","cases","certain","certainly","clear","clearly","come","could","couldn't","d","did","didn't","differ","different","differently","do","does","doesn't","doing","done","don't","down","downed","downing","downs","during","e","each","early","either","end","ended","ending","ends","enough","even","evenly","ever","every","everybody","everyone","everything","everywhere","f","face","faces","fact","facts","far","felt","few","find","finds","first","for","four","from","full","fully","further","furthered","furthering","furthers","g","gave","general","generally","get","gets","give","given","gives","go","going","good","goods","got","great","greater","greatest","group","grouped","grouping","groups","h","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","her","here","here's","hers","herself","he's","high","higher","highest","him","himself","his","how","however","how's","i","i'd","if","i'll","i'm","important","in","interest","interested","interesting","interests","into","is","isn't","it","its","it's","itself","i've","j","just","k","keep","keeps","kind","knew","know","known","knows","l","large","largely","last","later","latest","least","less","let","lets","let's","like","likely","long","longer","longest","m","made","make","making","man","many","may","me","member","members","men","might","more","most","mostly","mr","mrs","much","must","mustn't","my","myself","n","necessary","need","needed","needing","needs","never","new","newer","newest","next","no","nobody","non","noone","nor","not","nothing","now","nowhere","number","numbers","o","of","off","often","old","older","oldest","on","once","one","only","open","opened","opening","opens","or","order","ordered","ordering","orders","other","others","ought","our","ours","ourselves","out","over","own","p","part","parted","parting","parts","per","perhaps","place","places","point","pointed","pointing","points","possible","present","presented","presenting","presents","problem","problems","put","puts","q","quite","r","rather","really","right","room","rooms","s","said","same","saw","say","says","second","seconds","see","seem","seemed","seeming","seems","sees","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","show","showed","showing","shows","side","sides","since","small","smaller","smallest","so","some","somebody","someone","something","somewhere","state","states","still","such","sure","t","take","taken","than","that","that's","the","their","theirs","them","themselves","then","there","therefore","there's","these","they","they'd","they'll","they're","they've","thing","things","think","thinks","this","those","though","thought","thoughts","three","through","thus","to","today","together","too","took","toward","turn","turned","turning","turns","two","u","under","until","up","upon","us","use","used","uses","v","very","w","want","wanted","wanting","wants","was","wasn't","way","ways","we","we'd","well","we'll","wells","went","were","we're","weren't","we've","what","what's","when","when's","where","where's","whether","which","while","who","whole","whom","who's","whose","why","why's","will","with","within","without","won't","work","worked","working","works","would","wouldn't","x","y","year","years","yes","yet","you","you'd","you'll","young","younger","youngest","your","you're","yours","yourself","yourselves","you've","z")
-
-nyt1 <- nyt[,3]
-length(nyt1)
-dtm.control <- list(
-  tolower = T,
-  removePunctuation = T,
-  removeNumbers = T,
-  stopwords = c(stopwords("english"),extendedstopwords),
-  # snowball tem stopwords em pt
-  stemming = T,
-  wordLengths = c(3,Inf),
-  weighting = weightTf
-)
+# t termos com prob acima de minimo
+minimo <- .02
+terms(lda, t, threshold=minimo)
 
 
+
+# tópicos mais prováveis por documentos
+head(topics(lda), 10)
